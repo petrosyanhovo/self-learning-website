cbd31cd922aa49b7c55ccdccd9b1deba98914730
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2024-02-26 17:49:11 +0400

    Auth/login/logout

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p>This is a protected page</p>
-    
+    <div>
+      <UserButton
+        afterSignOutUrl='/'
+      />
+    </div>
   )
 }
