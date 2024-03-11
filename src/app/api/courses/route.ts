3340f1fdbde09277685a31afe7ab151a9d1e17be
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2024-03-11 22:31:30 +0400

    Resolved 500 Error

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -3,26 +3,27 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function POST(
-    req:Request
+    req:Request,
 ) {
     try {
-     const {userId} = auth();
-     const {title} = await req.json()
+        const { userId } = auth();
 
-    if(!userId) {
-        return new NextResponse("Unauthorized", {status: 401});
-    }
+        const { title } = await req.json();
 
-    const course = await db.course.create({
-        data: {
-            userId,
-            title,
+        if(!userId) {
+            return new NextResponse("Unauthorized", {status: 401})
         }
-    })
 
-    return NextResponse.json(course)
-    } catch(error) {
+        const course = await db.course.create({
+            data: {
+                userId,
+                title,
+            }
+        })
+
+        return NextResponse.json(course)
+    } catch (error) {
         console.log("[COURSES]", error);
-        return new NextResponse("Internal Error", {status: 500})
+        return new NextResponse("Internal Error", {status: 500});
     }
 }
